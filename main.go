e996d2b0dbff8d12e3c7d53bd9d3f8fb4a0a440d
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2020-04-08 12:36:56 +0100

    Creating main.go
    Added functionality to the main.go to enable a welcome page to webpage.
    This is backed up with a html page set and css formatting. This test is
    to prove that the inital code works this will be developed from this
    point to test adding more functionality onto the webpage
    
    files added
    main.go
    welcome-template.css
    welcome-template.html
    
    This displays a simple welcome page showing a welcome message with the
    time and date

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type Welcome struct {
+	Name string
+	Time string
+}
+
+func main() {
+
+	welcome := Welcome{"Jack", time.Now().Format(time.Stamp)}
+
+	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if name := r.FormValue("name"); name != "Jack" {
+			welcome.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("Listening")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
