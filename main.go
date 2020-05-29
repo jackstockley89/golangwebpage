da2347f89eb36fbe8fa10c092789f2a7f07290b0
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2020-05-29 14:27:38 +0100

    re-style of blog
    Add new style to the web layout including home and activities page to
    spilt from pervious layout of all being on one page
    
    To achive this created a new entry in the main.go file for a http
    Handler to allow a second page, Also added function for each page.
    
    Add HTML page for new page with own CSS, with a new look and layout

--- main.go
+++ main.go
@@ -12,25 +12,41 @@ type Welcome struct {
 	Time string
 }
 
-func main() {
+type Active struct {
+	Title string
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	welcome := Welcome{"to Cycling Blog", time.Now().Format(time.Stamp)}
 
-	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+	templates := template.Must(template.ParseFiles("templates/home.html"))
 
+	if name := r.FormValue("name"); name != "" {
+		welcome.Name = name
+	}
+
+	if err := templates.ExecuteTemplate(w, "home.html", welcome); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func activitiesHandler(w http.ResponseWriter, r *http.Request) {
+	p := Active{Title: "Activities"}
+	t := template.Must(template.ParseFiles("templates/activities.html"))
+	//t.Execute(w, p)
+
+	if err := t.ExecuteTemplate(w, "activities.html", p); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if name := r.FormValue("name"); name != "" {
-			welcome.Name = name
-		}
-
-		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
+	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/activities", activitiesHandler)
 	fmt.Println("Listening")
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
